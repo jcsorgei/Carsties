654533920ee5a4b1b5f5dc5ec5182b8550510b2e
Jozef Csorgei <csjozef88@gmail.com>
2023-12-27 19:45:48 +0100

    End of section 4

---
+++ src/SearchService/Consumers/AuctionUpdatedConsumer.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+
+namespace SearchService;
+
+public class AuctionUpdatedConsumer : IConsumer<AuctionUpdated>
+{
+    private readonly IMapper _mapper;
+
+    public AuctionUpdatedConsumer(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+    public async Task Consume(ConsumeContext<AuctionUpdated> context)
+    {
+        Console.WriteLine("--> Consuming auction updated: " + context.Message.Id);
+
+        var result = await DB.Update<Item>()
+            .Match(a => a.ID == context.Message.Id)
+            .ModifyOnly(x => new {x.Make, x.Model, x.Year, x.Color, x.Mileage},
+                 _mapper.Map<Item>(context.Message))
+            .ExecuteAsync();
+        
+        if (!result.IsAcknowledged)
+            throw new MessageException(typeof(AuctionUpdated), "Problem updating mongodb");
+    }
+}
