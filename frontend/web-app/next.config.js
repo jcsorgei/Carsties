eef0100a133c642e2802a40a87ab1e9186dc64a6
Jozef Csorgei <csjozef88@gmail.com>
2024-01-06 18:01:57 +0100

    End of section 8

---
+++ frontend/web-app/next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    logging: {
+        fetches: {
+            fullUrl: true
+        }
+    },
+    images: {
+        domains: [
+            'cdn.pixabay.com'
+        ]
+    }
+}
+
+module.exports = nextConfig
